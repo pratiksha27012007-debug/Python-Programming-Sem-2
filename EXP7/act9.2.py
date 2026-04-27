37f95c59adac50dbb38f112296502fd9b6e7a643
pratiksha27012007-debug <pratiksha27012007@gmail.com>
2026-04-27 15:38:27 +0530

    Add Employee class to calculate salary with bonus

---
+++ EXP7/act9.2.py
@@ -0,0 +1,31 @@
+# Create an Employee class that calculates salary with bonus.
+"""
+Created on Mon Apr 27 15:36:01 2026
+
+@author: pratiksha
+"""
+
+# Create an Employee class that calculates salary with bonus.
+"""
+Created on Mon Apr 13 15:16:02 2026
+
+@author: DIKSHA
+"""
+
+class Employee:
+    def __init__(self, name, salary, bonus_percentage):
+        self.name = name
+        self.salary = salary
+        self.bonus_percentage = bonus_percentage
+
+    def calculate_total(self):
+        bonus_amount = self.salary * (self.bonus_percentage / 100)
+        return self.salary + bonus_amount
+
+    def display(self):
+        total = self.calculate_total()
+        print(f"Employee: {self.name}")
+        print(f"Total Salary (with {self.bonus_percentage}% bonus): {total}")
+
+emp1 = Employee("Anagha", 50000, 20)
+emp1.display()
